5b0a45875a960264744f0d95dd1e838d8fe26d0b
Shailesh Aher <shaileshaher07@gmail.com>
2018-08-15 15:27:38 +0530

    Date format changed.

--- MerPayAssignment/MerPayAssignment/Helper/Constants.swift
+++ MerPayAssignment/MerPayAssignment/Helper/Constants.swift
@@ -14,7 +14,7 @@ typealias ResponseCallBack = ((_ success: Bool, _ response: Any?) -> Void)
 let URL_STRING = "https://api.github.com"
 let LOGGED_IN_USER = "johndoe"
 
-let DATE_DISPLAY_FORMAT = "yyyy-MM-dd"
+let DATE_DISPLAY_FORMAT = "dd MMM,"
 let TIME_DISPLAY_FORMAT = "hh:mm a"
 
 let IS_IPHONE_X =  UIDevice.current.userInterfaceIdiom == .phone && UIScreen.main.nativeBounds.height == 2436
